306f0d0197977235c04170a83767c900d0c824fb
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-01-04 17:06:08 +0100

    refactor : acceuil page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import Image from 'next/image';
+import Hero from '@/components/acceuil/Hero';
+import NosAteliers from '@/components/acceuil/NosAteliers';
 
 export default function Home() {
-  return <div>Setup</div>;
+  return (
+    <main>
+      <Hero />
+      <NosAteliers />
+    </main>
+  );
 }
