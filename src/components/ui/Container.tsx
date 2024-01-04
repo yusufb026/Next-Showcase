828718240d7b2314df83fd07dd3ef2e8f8d18632
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-01-04 01:42:33 +0100

    style : adjuste container padding

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -5,7 +5,7 @@ interface ConatinerProps {
 }
 
 const Container: FC<ConatinerProps> = ({ children }) => {
-  return <div className="w-ful mx-auto max-w-7xl">{children}</div>;
+  return <div className="w-ful mx-auto max-w-7xl  sm:px-10">{children}</div>;
 };
 
 export default Container;
